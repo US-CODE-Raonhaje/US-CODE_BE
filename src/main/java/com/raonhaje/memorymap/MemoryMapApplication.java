3442e23d3eae498835aeaf5dfccf2048da93c118
Seungwon-Choi <bestwondev@gmail.com>
2025-06-27 14:12:55 +0900

    feat: 스프링 부트 프로젝트 추가 #1

---
+++ src/main/java/com/raonhaje/memorymap/MemoryMapApplication.java
@@ -0,0 +1,13 @@
+package com.raonhaje.memorymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryMapApplication.class, args);
+    }
+
+}
