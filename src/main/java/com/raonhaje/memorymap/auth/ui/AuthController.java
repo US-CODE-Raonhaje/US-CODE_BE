adad864fff167ae78ba4d19bae646ba5ab455a0b
Seungwon-Choi <bestwondev@gmail.com>
2025-06-28 00:07:39 +0900

    feat: Mock API 응답 형태 AuthController 추가 #4

---
+++ src/main/java/com/raonhaje/memorymap/auth/ui/AuthController.java
@@ -0,0 +1,36 @@
+package com.raonhaje.memorymap.auth.ui;
+
+import com.raonhaje.memorymap.auth.dto.KakaoLoginRequest;
+import com.raonhaje.memorymap.auth.dto.TokenReissueRequest;
+import com.raonhaje.memorymap.auth.dto.TokenResponse;
+import com.raonhaje.memorymap.member.dto.MemberRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController implements AuthApiDocs {
+
+    @Override
+    public ResponseEntity<TokenResponse> kakaoLogin(KakaoLoginRequest request) {
+        return ResponseEntity.ok(new TokenResponse("mock accessToken", "mock refreshToken", true));
+    }
+
+    @Override
+    public ResponseEntity<Long> signUp(MemberRequest request) {
+        return ResponseEntity.ok(1L); // Mock response for sign-up
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> reissueAccessToken(TokenReissueRequest tokenRequest) {
+        return ResponseEntity.ok(new TokenResponse("mock newAccessToken", "mock newRefreshToken", true));
+    }
+
+    @Override
+    public ResponseEntity<Void> logout() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
