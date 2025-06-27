cb84b149a34b285e9000a78271ec9fdc43db1491
Seungwon-Choi <bestwondev@gmail.com>
2025-06-27 18:46:11 +0900

    feat: Member 엔티티 클래스 추가 #2

---
+++ src/main/java/com/raonhaje/memorymap/member/domain/Member.java
@@ -0,0 +1,40 @@
+package com.raonhaje.memorymap.member.domain;
+
+import com.raonhaje.memorymap.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String oauthId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String ageRange;
+
+    public static Member create(String oauthId, String nickname, String ageRange) {
+        return Member.builder()
+                .oauthId(oauthId)
+                .nickname(nickname)
+                .ageRange(ageRange)
+                .build();
+    }
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
