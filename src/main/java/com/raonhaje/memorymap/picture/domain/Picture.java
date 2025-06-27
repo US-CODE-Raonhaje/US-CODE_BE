04a8bf4318e153f737b5609aed280307100608f4
Seungwon-Choi <bestwondev@gmail.com>
2025-06-27 19:19:56 +0900

    feat: Picture 엔티티 클래스 추가 #2

---
+++ src/main/java/com/raonhaje/memorymap/picture/domain/Picture.java
@@ -0,0 +1,34 @@
+package com.raonhaje.memorymap.picture.domain;
+
+import com.raonhaje.memorymap.post.domain.Post;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "picture")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+public class Picture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "picture_id")
+    private Long pictureId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public static Picture create(Post post, String imageUrl) {
+        return Picture.builder()
+                .post(post)
+                .imageUrl(imageUrl)
+                .build();
+    }
+}
