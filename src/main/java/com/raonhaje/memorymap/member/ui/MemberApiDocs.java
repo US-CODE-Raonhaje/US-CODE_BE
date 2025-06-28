f94480025f0591af11dd511813c5dade13af53f1
Seungwon-Choi <bestwondev@gmail.com>
2025-06-28 13:52:30 +0900

    feat: 회원 관련 기능 추가 #17

--- src/main/java/com/raonhaje/memorymap/member/ui/MemberApiDocs.java
+++ src/main/java/com/raonhaje/memorymap/member/ui/MemberApiDocs.java
@@ -8,8 +8,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "회원 정보", description = "회원 정보 관련 API 문서입니다.")
 public interface MemberApiDocs {
@@ -21,5 +23,5 @@ public interface MemberApiDocs {
             @ApiResponse(responseCode = "401", description = "인증 실패")
     })
     @GetMapping("/me")
-    ResponseEntity<MemberInfoResponse> getMyInfo();
+    ResponseEntity<MemberInfoResponse> getMyInfo(@RequestHeader(name = "Authorization") String authorizationHeader);
 }
