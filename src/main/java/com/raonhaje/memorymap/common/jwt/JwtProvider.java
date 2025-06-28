153898012ff2bc0ecc3ae3e20bdd35a9e8a8e61f
Seungwon-Choi <bestwondev@gmail.com>
2025-06-29 02:23:48 +0900

    refactor: JwtProvider 환경 변수 설정 변경

--- src/main/java/com/raonhaje/memorymap/common/jwt/JwtProvider.java
+++ src/main/java/com/raonhaje/memorymap/common/jwt/JwtProvider.java
@@ -3,32 +3,48 @@ package com.raonhaje.memorymap.common.jwt;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtProvider {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    private final long accessTokenValidity = 1000 * 60 * 30; // 30분
-    private final long refreshTokenValidity = 1000L * 60 * 60 * 24 * 7; // 7일
+    @Value("${JWT_SECRET}")
+    private String secretKey;
+
+    @Value("${ACCESS_TOKEN_EXPIRATION_MS}")
+    private long accessTokenExpirationMs;
+
+    @Value("${REFRESH_TOKEN_EXPIRATION_MS}")
+    private long refreshTokenExpirationMs;
+
+    private Key key;
+
+    @PostConstruct
+    protected void init() {
+        byte[] keyBytes = Base64.getEncoder().encode(secretKey.getBytes());
+        this.key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+    }
 
     public String createAccessToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
-                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
-                .signWith(key)
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String createRefreshToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
-                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidity))
-                .signWith(key)
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
