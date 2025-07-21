75038c996caf6021f489f7618bd752e9fdd7989a
Seungwon-Choi <bestwondev@gmail.com>
2025-07-21 17:38:07 +0900

    refactor: 회원 등록, 조회 API 리팩터링 #33

--- src/main/java/com/raonhaje/memorymap/member/application/MemberService.java
+++ src/main/java/com/raonhaje/memorymap/member/application/MemberService.java
@@ -4,7 +4,6 @@ import com.raonhaje.memorymap.common.exception.BusinessException;
 import com.raonhaje.memorymap.common.exception.ErrorCode;
 import com.raonhaje.memorymap.member.domain.Location;
 import com.raonhaje.memorymap.member.domain.Member;
-import com.raonhaje.memorymap.member.dto.MemberInfoResponse;
 import com.raonhaje.memorymap.member.dto.MemberSignUpRequest;
 import com.raonhaje.memorymap.member.repository.MemberJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,27 +11,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberJpaRepository memberJpaRepository;
 
-    public MemberInfoResponse getMemberInfo(String email) {
-        return memberJpaRepository.findByEmail(email)
-                .map(MemberInfoResponse::from)
-                .orElseThrow(() -> new IllegalArgumentException("Member not found with email: " + email));
-    }
-
     @Transactional
-    public Member signUp(String email, MemberSignUpRequest request) {
-        Member member = memberJpaRepository.findByEmail(email)
-                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+    public Member signUp(MemberSignUpRequest request) {
+        if (memberJpaRepository.existsByEmail(request.email())) {
+            throw new BusinessException(ErrorCode.EXISTING_EMAIL);
+        }
 
-        member.setAdditionalInfo(request.nickname(),
-                request.age(),
-                request.profileImageUrl(),
-                Location.create(request.latitude(), request.longitude(), request.address()));
+        if (memberJpaRepository.existsByNickname(request.nickname())) {
+            throw new BusinessException(ErrorCode.EXISTING_NICKNAME);
+        }
+
+        Location location = Location.create(request.latitude(), request.longitude(), request.address());
+        Member member = Member.create(request.email(), request.name(), request.nickname(), request.age(), request.profileImageUrl(), location);
 
         return memberJpaRepository.save(member);
     }
