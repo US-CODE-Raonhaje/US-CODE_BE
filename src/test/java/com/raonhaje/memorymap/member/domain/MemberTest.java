048703f3956edfbef2353e714f5f64a24eda9f71
Seungwon-Choi <bestwondev@gmail.com>
2025-07-21 17:39:27 +0900

    test: Member 생성 테스트 코드 수정 #33

--- src/test/java/com/raonhaje/memorymap/member/domain/MemberTest.java
+++ src/test/java/com/raonhaje/memorymap/member/domain/MemberTest.java
@@ -12,21 +12,31 @@ class MemberTest {
     void createMemberTest() {
         // given
         String email = "testEmail@gmail.com";
-        String name = "testname";
+        String name = "testName";
+        String nickname = "testNickname";
+        int age = 25;
+        String imageUrl = "testImageUrl";
+
+        Location location = Location.create(37.5665, 126.9784, "서울특별시 마포구");
 
         // when
-        Member member = Member.create(email, name);
+        Member member = Member.create(email, name, nickname, age, imageUrl, location);
 
         // then
         assertEquals(email, member.getEmail());
         assertEquals(name, member.getName());
+        assertEquals(nickname, member.getNickname());
+        assertEquals(age, member.getAge());
+        assertEquals(imageUrl, member.getImageUrl());
+        assertEquals(location, member.getLocation());
     }
 
     @Test
     @DisplayName("Member 업데이트 테스트")
     void updateMemberTest() {
         // given
-        Member member = Member.create("testEmail@gmail.com", "testNickname");
+        Location location = Location.create(37.5665, 126.9784, "서울특별시 마포구");
+        Member member = Member.create("testEmail@gmail.com", "testName", "testNickname", 25, "testImageUrl", location);
         String newNickname = "updatedNickname";
         String newImageUrl = "http://example.com/image.jpg";
 
