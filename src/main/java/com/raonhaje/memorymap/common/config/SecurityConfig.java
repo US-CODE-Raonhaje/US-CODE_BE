b2b70ee25444a34e5e236bc87b91ac64c888bdf6
Seungwon-Choi <bestwondev@gmail.com>
2025-07-25 14:41:56 +0900

    fix: 시큐리티 requestMatchers 수정

--- src/main/java/com/raonhaje/memorymap/common/config/SecurityConfig.java
+++ src/main/java/com/raonhaje/memorymap/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -34,16 +35,21 @@ public class SecurityConfig {
                                 "/v2/api-docs/**",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
-                                "/webjars/**"
+                                "/webjars/**",
+                                "/h2-console/**",
+                                "/api/**" // TODO: API 개발 완료 후, 이 경로를 제거하거나 수정해야 합니다.
                         ).permitAll()
-                        .anyRequest().permitAll() // TODO: API 개발 완료 후, authenticated()로 변경
+                        .anyRequest().authenticated()
                 )
+                .headers( headers ->
+                    headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                );
 //                .oauth2Login(oauth2 -> oauth2
 //                        .userInfoEndpoint(userInfo -> userInfo
 //                                .userService(customOAuth2UserService)
 //                        )
 //                        .successHandler(oAuth2SuccessHandler)
-                ;
+
         return http.build();
     }
 
